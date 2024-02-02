a9a3a0c941b8780638e9e1cb91ae6d47451657ad
chief-khanman <farhanaadit@gmail.com>
2024-02-02 17:36:54 -0600

    Wrote skeleton code for animation in main.py.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import geopandas as gpd
+import time
 
 
 if __name__ == '__main__':
@@ -24,23 +25,41 @@ if __name__ == '__main__':
     # print("Flight completed.")
     # print(len(path_trace))
     # print(type(path_trace[0]))
-        
+    plt.ion() 
     fig, ax = plt.subplots()
-    airspace.location_utm_gdf.plot(ax=ax, color='blue', linewidth=0.6)
-    airspace.location_utm_hospital_buffer.plot(ax=ax, color='red', alpha=0.3)
-    airspace.location_utm_hospital.plot(ax=ax, color='black')
+    start_end_point = gpd.GeoSeries([start_point, end_point])
+    # airspace.location_utm_gdf.plot(ax=ax, color='blue', linewidth=0.6)
+    # airspace.location_utm_hospital_buffer.plot(ax=ax, color='red', alpha=0.3)
+    # airspace.location_utm_hospital.plot(ax=ax, color='black')
+    def static_plot():
+        airspace.location_utm_gdf.plot(ax=ax, color='blue', linewidth=0.6)
+        airspace.location_utm_hospital_buffer.plot(ax=ax, color='red', alpha=0.3)
+        airspace.location_utm_hospital.plot(ax=ax, color='black')
+        start_end_point.plot(ax=ax, color='black')
     
     #Plot start and end point
-    start_end_point = gpd.GeoSeries([start_point, end_point])
-    start_end_point.plot(ax=ax, color='black')
+    # start_end_point = gpd.GeoSeries([start_point, end_point])
+    # start_end_point.plot(ax=ax, color='black')
     
-    #TODO - fix this mess 
+     
+    #* Using FuncAnimation to animate the path of the UAV
+    # def animate(frame):
+    #     gpd.GeoSeries([path_trace[frame]]).plot(ax=ax, color='red', alpha=0.3)
+    
+    # ani = FuncAnimation(fig, animate, frames=len(path_trace), interval=100, repeat=False)
+    # plt.show()
 
-    def animate(frame):
-        gpd.GeoSeries([path_trace[frame]]).plot(ax=ax, color='red', alpha=0.3)
-    
-    ani = FuncAnimation(fig, animate, frames=len(path_trace), interval=100, repeat=False)
-    plt.show()
+    #* Using for loop to animate the path of the UAV - without trace
+    static_plot() 
+    for i in range(len(path_trace)):
+        plt.cla()
+        static_plot()
+        gpd.GeoSeries([path_trace[i]]).plot(ax=ax, color='red', alpha=0.3)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        time.sleep(0.1)
+
+
     
     
 

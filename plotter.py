43674e62650cc3a4b9d88f42e90432d764346ba7
chief-khanman <farhanaadit@gmail.com>
2024-02-16 12:52:20 -0600

    added methods to plotter, but logic is not correct.

--- plotter.py
+++ plotter.py
@@ -4,34 +4,21 @@ from geopandas import GeoSeries
 from airspace import Airspace
 import uav
 import airtrafficcontroller
+import simulator
 
 
-# Plotter will access the path traces of uavs in airspace, 
-# It will have a method that 
+class Plotter:
+    def __init__(self, sim_instance):
+        self.sim_instance = sim_instance
+        self.vertiport_gs = GeoSeries(self.sim_instance.vertiports)
+        self.fig, self.ax = plt.subplots()
 
 
+    def static_plot(self, ):
+        self.sim_instance.airspace.location_utm_gdf.plot(ax=self.ax, color='gray', linewidth = 0.6)
+        self.sim_instance.airspace.location_utm_hospital_buffer.plot(ax=self.ax, color='green', alpha=0.3)
+        self.sim_instance.airspace.location_utm_hospital.plot(ax=self.ax, color='black')
+        self.vertiport_gs.plot(ax=self.ax, color = 'black')
 
-plt.ion()
-fig, ax = plt.subplots() #have three axes, i)map ii)NMAC count iii)collision count
-start_end_array = []
-
-
-def plot_vertiports(vertiports):
-    ax.plot(vertiports)
-
-
-def plot_static_objects(airspace:Airspace):
-    airspace.location_utm_gdf.plot(ax=ax, color='blue', linewidth=0.6)
-    airspace.location_utm_hospital_buffer.plot(ax=ax, color='red', alpha=0.3)
-    airspace.location_utm_hospital.plot(ax=ax, color='black')
-
-def plotter():
-    #TODO - collect path traces of all uavs in airspace
-    #TODO - find shortest path_trace length -> loop_len
-    #TODO - loop with loop_len and plot the uav path_traces on map 
-
-
-
-    #TODO - COMPLETE THE PLOTTING RIGHT NOW, THINK OF EVERYTHING LATER 
-    #TODO - plotting 
-    #TODO - 
+    def interactive_plotting_on(self):
+        return plt.ion()

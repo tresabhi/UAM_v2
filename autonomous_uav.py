fffb89ae712c9f85a72b54385fe9ad3fb575711d
chief-khanman <farhanaadit@gmail.com>
2024-02-16 12:51:47 -0600

    Created autonomous uav module

--- autonomous_uav.py
+++ autonomous_uav.py
@@ -4,7 +4,6 @@ from uav import UAV
 class Autonomous_UAV(UAV):
     '''Sub class of UAV, a smart UAV .
     It will accept acceleration and heading from RL algorithm'''
-    pass
 
     def __init__(self,):
         pass

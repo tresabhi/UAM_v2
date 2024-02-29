ba687d80534faf72c6807ec52ff1c4b564fe5ba4
chief-khanman <farhanaadit@gmail.com>
2024-02-29 15:19:13 -0600

    vertiport is just a simple data object that keeps track of uavs have interacted with it. Returned vertiport to its simple and plain state.

--- vertiport.py
+++ vertiport.py
@@ -1,25 +1,4 @@
 '''
-when ATC creates vetiports, create instances of vertiports at those locations, 
-ATC also knows how many uavs I have in my system, 
-UAVs are assigned to vertiports, 
-Vertiports keep count of UAVs at its disposal 
-Vertiport releases UAV at some random time only if it has UAVs 
-A UAV is added to the Vertiport, when it arrives 
-same UAV can be released with new end point from given Vertiport 
-'''
-
-'''The simulator - 
-
-    maintains time, 
-    Time starts at 0, continues till end(defined by user)
-    
-    How long should a simulation run, what determines the end of simulation
-
-
-    Vanilla simulator runs - for a specified time defined by the user. 
-                           - Understand what is steps, 
-
-
     FOR Training the RL algorithm - the simulation should run until the Auto_UAV reaches its endpoint
                                   - so simulation ends when Auto_UAV reaches its endpoint 
                                   - We run N number of simulations 
@@ -27,15 +6,17 @@ same UAV can be released with new end point from given Vertiport
     '''
 
 
-'''
-every class is one object - every function does only one thing for that object 
+'''every class is one object - every function does only one thing for that object '''
 
-UAV - needs -  start, end, 
+from shapely import Point
+from typing import List
 
-Airspace  - needs - location name
 
-ATC - needs to know - location of all UAV, location of all vertiport 
+class Vertiport:
+    def __init__(self,location, uav_list=[]): #sim:simulator.Simulator,
+        self.id = id(self)
+        self.location = location
+        self.uav_list:List = uav_list
 
-Vertiport - needs - location of vertiport, no of uav at station 
-
-'''
+    # def __repr__(self,):
+    #     return 'Vertiport({location}, {uav_list})'.format(location=self.location, uav_list=self.uav_list)

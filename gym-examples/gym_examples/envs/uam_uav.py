61f17828a96be97b4d4b3b21c5fdfe588cb3f37f
chief-khanman <farhanaadit@gmail.com>
2024-05-14 20:07:53 -0500

    updated uam_uav for matching gym env

--- gym-examples/gym_examples/envs/uam_uav.py
+++ gym-examples/gym_examples/envs/uam_uav.py
@@ -9,12 +9,19 @@ from wrappers import airspace, airtrafficcontroller
 uam_airspace = airspace.Airspace('Austin, Texas, USA')
 uam_atc = airtrafficcontroller.ATC(airspace=uam_airspace)
 
+#! Warning
+'''
+Remember this environment will only be used for training,
+Therefore there will only be one auto_uav which is built into the uam_env 
+'''
+#! End 
 
 
 class Uam_Uav_Env(gym.Env):
     metadata = {"render_mode":["human", "rgb_array"], "render_fps":4}
 
-    def __init__(self, airspace,airtrafficcontroller,uav,autonomous_uav,vertiport,render_mode=None):
+    def __init__(self, location_name, num_vertiport, num_reg_uav,render_mode=None):
+        #airspace,airtrafficcontroller,uav,autonomous_uav,vertiport,
         pass 
 
     def _get_obs(self,):
@@ -27,5 +34,14 @@ class Uam_Uav_Env(gym.Env):
         pass 
 
     def step(self,action):
+        pass
+
+    def render(self,):
+        pass
+
+    def _render_frame(self,):
+        pass
+
+    def close(self,):
         pass
     

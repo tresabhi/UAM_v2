51147525f91d002f6ec8de4a0b53b2de1eeb4213
chief-khanman <farhanaadit@gmail.com>
2024-05-28 17:30:35 -0500

    updated collision_controller to take into account static object detection and avoidance

--- gym-examples/gym_examples/envs/assets/das.py
+++ gym-examples/gym_examples/envs/assets/das.py
@@ -43,15 +43,22 @@ class Collision_controller:
         else:
             raise RuntimeError('DAS Error: Invalid heading')
         
-
-    def get_action(self, state):
-        if state == None:
-            return 0,0
-        else:
-            own_pos = state['own_pos']
-            int_pos = state['intruder_pos']
-            own_heading = state['own_current_heading']
-            int_heading = state['intruder_current_heading']
+    '''
+    state -> static state                dynamic state                          get_action
+            False                           None                                    
+    '''
+    #                    state -> ((bool,                float                  ), dict            | None)
+    #                               static_obj_detected, uav.current_heading_deg , dynamic_obj_info| None
+    def get_action(self, state): 
+        if state[0][0] is False and state[1] is None:
+            acceleration = 0
+            heading_correction = 0
+        
+        elif state[0][0] is False and isinstance(state[1], dict):
+            own_pos = state[1]['own_pos']
+            int_pos = state[1]['intruder_pos']
+            own_heading = state[1]['own_current_heading']
+            int_heading = state[1]['intruder_current_heading']
 
             del_x = int_pos.x - own_pos.x
             del_y = int_pos.y - own_pos.y
@@ -87,8 +94,31 @@ class Collision_controller:
                     acceleration = 0
             else:
                 raise RuntimeError('Action not from scenario')
+        
+        elif state[0][0] is True and state[1] is None:
+            acceleration = 0 
+            current_heading = state[0][1]
+            
+            if (current_heading < 0):
+                current_heading += 360
+            elif (current_heading > 180):
+                current_heading -= 360
 
-            return acceleration, heading_correction
+            if 0 <= current_heading or current_heading <= 180:
+                heading_correction = 25
+            elif -180<=current_heading or current_heading<= 0: 
+                heading_correction = -25
+            else:
+                raise RuntimeError(f'DAS module - state[0][0] is True and state[1] is None, current heading {state[0][1]}')
+
+        elif state[0][0] is True and isinstance(state[1], dict):
+            acceleration = 0
+            heading_correction = 0
+        
+        else:
+            raise RuntimeError('DAS module: static and dynamic states do not match the conditionals')
+
+        return acceleration, heading_correction
     
 
 

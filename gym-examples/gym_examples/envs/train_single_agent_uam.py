d5f1ad6cdf36c634eb6f62fb5abffa11b96b5a16
chief-khanman <farhanaadit@gmail.com>
2024-07-09 11:25:59 -0500

    created check_env and training script for single agent env

--- gym-examples/gym_examples/envs/train_single_agent_uam.py
+++ gym-examples/gym_examples/envs/train_single_agent_uam.py
@@ -1,18 +1,27 @@
+'''
+Use this script to train a model using the custom UAM environment.
+Before training, make sure to run check_uam.py to ensure the environment is in working condition. 
+This check must be done anytime there is a change to the environment.
+
+Once the check is complete you can proceed with training a model using algorithms from sb3
+'''
+
+
+
 # from stable_baselines3 import A2C
 import gymnasium as gym
-from stable_baselines3.common.env_checker import check_env
 import matplotlib.pyplot as plt
 # Register your custom environment
 from gymnasium.envs.registration import register
 from uam_uav import Uam_Uav_Env
 
 env = Uam_Uav_Env('Austin, Texas, USA', 8, 5, sleep_time=0.01)
-#check_env(env)
 obs, info = env.reset()
 
-
+# To have interactive plotting active 
 plt.ion()
 fig, ax = env.render_init()
+
 for _ in range(1500):
     action = env.action_space.sample()
     env.render(fig, ax)

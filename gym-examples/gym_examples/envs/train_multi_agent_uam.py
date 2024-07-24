166182af441ad21f4f8522e2e2ce8dca345664c1
chief-khanman <farhanaadit@gmail.com>
2024-07-23 19:09:51 -0500

    multi_agent env checker script and training script completed

---
+++ gym-examples/gym_examples/envs/train_multi_agent_uam.py
@@ -0,0 +1,17 @@
+from multi_uav_uam import Uam_Uav_Env_PZ
+import matplotlib.pyplot as plt
+
+parallel_env = Uam_Uav_Env_PZ('Austin, Texas, USA', 8, 5, sleep_time=0.01)
+observations, infos = parallel_env.reset(seed=42)
+
+
+# To have interactive plotting active 
+plt.ion()
+fig, ax = parallel_env.render_init()
+
+
+while parallel_env.agents:
+    # this is where you would insert your policy
+    actions = {agent: parallel_env.action_space(agent).sample() for agent in parallel_env.agents}
+    parallel_env.render(fig, ax)
+    observations, rewards, terminations, truncations, infos = parallel_env.step(actions)

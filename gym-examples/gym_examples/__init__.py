3f16cbc2f34db9cf79bd47f8873b2aadf0bb7126
Nathan Van Utrecht <nvan21@iastate.edu>
2024-07-29 13:54:05 -0500

    Changed gym registration to use UamUavEnv as the entry point

--- gym-examples/gym_examples/__init__.py
+++ gym-examples/gym_examples/__init__.py
@@ -1,7 +1,7 @@
 from gymnasium.envs.registration import register
 
 register(
-     id="gym_examples/UAM-v0",
-     entry_point="gym_examples.envs:Uam_Uav_Env",
-     max_episode_steps=10_000,
+    id="gym_examples/UAM-v0",
+    entry_point="gym_examples.envs:UamUavEnv",
+    max_episode_steps=10_000,
 )

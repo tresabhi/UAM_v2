3718dd68524d44266d80e05e81e588c6ab961c71
Nathan Van Utrecht <nvan21@iastate.edu>
2024-07-29 14:56:09 -0500

    Added airspace_test.py which creates an airspace and prints out the attributes. Not sure what else needs to be added for it

--- gym-examples/gym_examples/test_scripts/airspace_test.py
+++ gym-examples/gym_examples/test_scripts/airspace_test.py
@@ -1,10 +1,18 @@
-from ..envs.airspace import Airspace
+import sys
+import os
+
+# Add the parent directory of 'gym_examples' to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+from envs.airspace import Airspace
 
 if __name__ == "__main__":
     location_name = "Austin, Texas, USA"
     buffer_radius = 500
 
     airspace = Airspace(location_name=location_name, buffer_radius=buffer_radius)
+    airspace_attributes = vars(airspace)
 
-    for attribute, value in vars(airspace):
-        print(attribute, value)
+    for attribute, value in airspace_attributes.items():
+        print(f"Attribute: '{attribute}', Type: '{type(attribute)}'")

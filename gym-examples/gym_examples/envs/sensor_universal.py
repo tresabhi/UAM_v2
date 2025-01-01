614540575be0ef9596d70cf779dbb8d7990f86e4
chief-khanman <farhanaadit@gmail.com>
2025-01-02 00:42:44 +0600

    added new methods for retriving nmac and collision

--- gym-examples/gym_examples/envs/sensor_universal.py
+++ gym-examples/gym_examples/envs/sensor_universal.py
@@ -27,3 +27,30 @@ class UniversalSensor(SensorTemplate):
     def get_data(self):
         '''Return data of other UAVs in space'''
         return super().get_data()
+    
+
+    def get_collision(self, self_uav:UAV_v2):
+        uav_list = self.space.get_uav_list()
+        
+        for uav in uav_list:
+            if uav.id == self_uav.id:
+                continue
+            else:
+                if self_uav.current_position.buffer(self_uav.radius).intersects(uav.current_position.buffer(uav.radius)):
+                    return True, (self_uav.id, uav.id)
+        return False, None
+
+    def get_nmac(self, self_uav:UAV_v2):
+        nmac_list = []
+        uav_list = self.space.get_uav_list()
+
+        for uav in uav_list:
+            if uav.id == self_uav.id:
+                continue
+            else:
+                if self_uav.current_position.buffer(self_uav.nmac_radius).intersects(uav.current_position.buffer(uav.nmac_radius)):
+                    nmac_list.append(uav)
+        if len(nmac_list) > 0:
+            return True, nmac_list
+        return False, nmac_list
+            

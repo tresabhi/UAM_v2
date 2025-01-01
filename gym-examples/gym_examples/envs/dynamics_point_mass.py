42cd09f207dcb106c5a637a07d7eb6b7976eb240
chief-khanman <farhanaadit@gmail.com>
2025-01-02 00:48:10 +0600

    fixed the dynamics equations

--- gym-examples/gym_examples/envs/dynamics_point_mass.py
+++ gym-examples/gym_examples/envs/dynamics_point_mass.py
@@ -1,6 +1,8 @@
+import math
 from dynamics_template import DynamicsTemplate
 from shapely import Point
 from uav_v2_template import UAV_v2_template
+
 
 class PointMassDynamics(DynamicsTemplate):
 
@@ -21,8 +23,14 @@ class PointMassDynamics(DynamicsTemplate):
             new_speed = uav.current_speed + action[0] * self.dt
             new_heading = uav.current_heading + action[1]
 
-            _x = uav.current_position.x + (uav.current_speed * self.dt) + (0.5 * action[0] * self.dt**2)
-            _y = uav.current_position.y + (uav.current_speed * self.dt) + (0.5 * action[0] * self.dt**2)
+            v_x = uav.current_speed * math.cos(new_heading)
+            a_x = action[0] * math.cos(new_heading)
+            
+            v_y = uav.current_speed * math.sin(new_heading)
+            a_y = action[0] * math.sin(new_heading)
+
+            _x = uav.current_position.x + (v_x * self.dt) + (0.5 * a_x * self.dt**2)
+            _y = uav.current_position.y + (v_y * self.dt) + (0.5 * a_y * self.dt**2)
 
             uav.current_position = Point(_x, _y)
             uav.current_speed = new_speed

1653beb3254c417c3382fd788369a89311fc59b6
chief-khanman <farhanaadit@gmail.com>
2025-01-10 21:58:00 +0600

    updated method definitions get_data, set_data

--- gym-examples/gym_examples/envs/sensor_universal.py
+++ gym-examples/gym_examples/envs/sensor_universal.py
@@ -1,6 +1,10 @@
 from sensor_template import SensorTemplate
+from typing import List
 from pandas import DataFrame
-from uav_v2 import UAV_v2
+import numpy as np
+import shapely
+from uav_v2_template import UAV_v2_template
+from utils import compute_time_to_impact
 
 class UniversalSensor(SensorTemplate):
 
@@ -8,28 +12,74 @@ class UniversalSensor(SensorTemplate):
         super().__init__(space)
 
 
-    def set_data(self, self_uav:UAV_v2) -> None:
+    def set_data(self, self_uav:UAV_v2_template) -> None:
         '''Collect data of other UAVs in space'''
-        uav_list = self.space.get_uav_list()
-        self_uav_state = self_uav.get_state()
+        
+        self.detection_radius = self_uav.detection_radius
+        
+        uav_list:List[UAV_v2_template] = self.space.get_uav_list()
+        self_uav_ref_prll, self_uav_ref_orth = self_uav.get_ref()
 
         for uav in uav_list:
             # collect information about other UAV
             if uav.id != self_uav.id:
-                uav_state = uav.get_state()
-                relative_distance = self_uav_state['current_position'] - uav_state['current_position']
-                relative_heading = self_uav_state['current_heading'] = uav_state['current_heading']
-                other_uav_state = {'id':uav_state['id'],'start':uav_state['start'], 'end':uav_state['end'], 'relative_distance':relative_distance, 'relative_heading':relative_heading}
+                relative_distance =  np.array([uav.current_position.x - self_uav.current_position.x, uav.current_position.y - self_uav.current_position.y])
+                p_parallel_ego_frame = np.dot(relative_distance, self_uav_ref_prll)
+                p_orthogonal_ego_frame = np.dot(relative_distance, self_uav_ref_orth)
+                v_parallel_ego_frame = np.dot(np.array([uav.current_speed*np.cos(uav.current_heading), uav.current_speed*np.sin(uav.current_heading)]), self_uav_ref_prll)
+                v_orthogonal_ego_frame = np.dot(np.array([uav.current_speed*np.cos(uav.current_heading), uav.current_speed*np.sin(uav.current_heading)]), self_uav_ref_orth)
+                dist_between_agent_centers = shapely.distance(self_uav.current_position, uav.current_position)
+                dist_to_other = round(dist_between_agent_centers - self_uav.radius - uav.radius, 2)
+                combined_radius = self_uav.radius + uav.radius
+                # relative_heading = self_uav_state['current_heading'] = uav_state['current_heading']
+                time_to_impact = compute_time_to_impact(self_uav, uav)
+                obs_array = np.array([p_parallel_ego_frame,
+                                      p_orthogonal_ego_frame,
+                                      v_parallel_ego_frame,
+                                      v_orthogonal_ego_frame,
+                                      uav.radius,
+                                      combined_radius,
+                                      dist_to_other,
+                                      time_to_impact])
                 # save information in data
-                self.data.append(other_uav_state)
+                self.data.append(obs_array)
         return None
     
-    def get_data(self):
-        '''Return data of other UAVs in space'''
-        return super().get_data()
+    
+    def get_data(self, sorting_criteria: str):
+        """Return data of other UAVs in space.
+
+        Args:
+            sorting_criteria (str): Sorting method, one of ['closest first', 'closest last', 'time of impact'].
+
+        Returns:
+            np.ndarray: Sorted data with fixed shape (max_number_other_agents_observed, 7).
+        """
+        if sorting_criteria == 'closest first':
+            sorted_data = sorted(self.data, key=lambda x: (x[6], x[1]))
+        elif sorting_criteria == 'closest last':
+            sorted_data = sorted(self.data, key=lambda x: (x[6], x[1]), reverse=True)
+        elif sorting_criteria == 'time of impact':
+            sorted_data = sorted(self.data, key=lambda x: (-x[7], -x[6], x[1]))
+        else:
+            raise RuntimeError('Not a valid sorting_criteria')
+
+        # Convert to numpy array for consistent shape handling
+        sorted_data = np.array(sorted_data)
+
+        # Truncate or pad to ensure fixed shape
+        if len(sorted_data) > self.max_number_other_agents_observed:
+            # Truncate extra rows
+            sorted_data = sorted_data[:self.max_number_other_agents_observed]
+        elif len(sorted_data) < self.max_number_other_agents_observed:
+            # Pad with zeros if there are fewer rows
+            padding = np.zeros((self.max_number_other_agents_observed - len(sorted_data), 7))
+            sorted_data = np.vstack((sorted_data, padding))
+
+        return sorted_data
     
 
-    def get_collision(self, self_uav:UAV_v2):
+    def get_collision(self, self_uav:UAV_v2_template):
         uav_list = self.space.get_uav_list()
         
         for uav in uav_list:
@@ -40,7 +90,7 @@ class UniversalSensor(SensorTemplate):
                     return True, (self_uav.id, uav.id)
         return False, None
 
-    def get_nmac(self, self_uav:UAV_v2):
+    def get_nmac(self, self_uav:UAV_v2_template):
         nmac_list = []
         uav_list = self.space.get_uav_list()
 

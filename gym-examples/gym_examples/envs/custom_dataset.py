9733aa22701db1c910f41720c58b68bf11cb00cd
chief-khanman <farhanaadit@gmail.com>
2025-02-10 10:20:18 -0600

    added method definition for custom dataset script

--- gym-examples/gym_examples/envs/custom_dataset.py
+++ gym-examples/gym_examples/envs/custom_dataset.py
@@ -1,5 +1,6 @@
 import os 
 import torch 
+import pandas as pd
 from torch.utils.data import Dataset
 
 
@@ -8,14 +9,18 @@ class CustomTrainingDataset(Dataset):
         This script will load data stored in directory. 
         The data is used for pretraining the embedding(RNN/GNN) and RL model.
     '''
-    def __init__(self,input, output, transform=None, target_transform=None):
-        # provide the location of input data
-        # provide the location of output data
-        pass
+    def __init__(self,data_file, transform=None, target_transform=None):
+        self.data_frame = pd.read_csv(data_file) 
+        self.transform = transform
+        self.target_transform = target_transform
+        
 
     def __len__(self):
-        pass
+        return len(self.data_frame)
 
-    def __getitem__(self): # DOES THIS METHOD ONLY ACCEPT ONE ARGUMENT - idx
-                           # can this method accept more than one argument
-        pass
+
+    def __getitem__(self, idx):
+        input_data = self.data_frame.iloc[idx, 0]
+        output_data = self.data_frame.iloc[idx, 1]
+        return input_data, output_data
+        

6b113fb47c00b1277b34ad2cde7eb81ba94405a1
chief-khanman <farhanaadit@gmail.com>
2025-03-20 20:59:00 -0500

    added scripts for ORCA dynamics and controller

--- gym-examples/gym_examples/envs/controller_non_coop_ORCA.py
+++ gym-examples/gym_examples/envs/controller_non_coop_ORCA.py
@@ -1,146 +1,77 @@
-from __future__ import division
+from typing import Tuple, List, Dict
+import numpy as np
 
 from controller_template import ControllerTemplate
-from uav_v2 import UAV_v2_template
-import numpy
-from numpy import array, sqrt, copysign, dot
-from numpy.linalg import det
-from halfplaneintersect import halfplane_optimize, Line, perp
+
+import pyorca as ORCA
 
 class ORCA_controller(ControllerTemplate):
+    
     def __init__(self, max_acceleration, max_heading_change, tau, dt):
         super().__init__(max_acceleration, max_heading_change)
         self.tau = tau
         self.dt = dt
 
-    def __call__(self, observation):
-        agent = observation[0]
-        candidates = observation[1:]
-        new_vel, all_lines = self.orca(agent, candidates, self.tau, self.dt)
-
-        action = new_vel
-        
-        return action
     
-    def orca(self, agent:UAV_v2_template, colliding_agents, t, dt):
-            """Compute ORCA solution for agent. NOTE: velocity must be _instantly_
-            changed on tick *edge*, like first-order integration, otherwise the method
-            undercompensates and you will still risk colliding."""
-            lines = []
-            agent_current_speed = agent.current_speed
-            agent_current_heading = agent.current_heading
-            agent_vx = agent_current_speed * numpy.cos(agent_current_heading)
-            agent_vy = agent_current_speed * numpy.sin(agent_current_heading)
-            pref_velocity = array(agent_vx,agent_vy)
-            
-            for collider in colliding_agents:
-                dv, n = self.get_avoidance_velocity(agent, collider, t, dt)
-                line = Line(agent.velocity + dv / 2, n)
-                lines.append(line)
+    
+    
+    
+    
+    def __call__(self, observation:List[Dict]) -> Tuple: #action
+       '''
+       input: observation list of dicts, each dict consists of agent information. 
+       The first dict consists of self information. 
 
-            return halfplane_optimize(lines, agent.pref_velocity), lines
+       Return: action tuple. 
+       '''
+       # creating Agent according to pyORCA's implementation
+       SPEED = 15 
+       _agent = observation[0]
+       _agent_position = _agent['current_position'].x, _agent['current_position'].y
+       _agent_speed = SPEED #_agent['current_speed']
+       _agent_heading = _agent['current_heading']
+       _agent_velocity = [_agent_speed*np.cos(_agent_heading), 
+                          _agent_speed*np.sin(_agent_heading)]
+       
+       ref_final_point = _agent['current_position'] - _agent['end'] #Point obj
+       x = np.array([ref_final_point.x, ref_final_point.y])
+       pref_vel = ORCA.normalized(-x) * _agent_speed
+       
+       print(f'speed: {_agent_speed}, heading: {_agent_heading}')
+
+
+       agent = ORCA.Agent(position=_agent_position, 
+                          velocity=_agent_velocity,
+                          radius=_agent['radius'],
+                          max_speed=_agent['max_speed'],
+                          pref_velocity= pref_vel)
+       
+       candidates = []
+       
+       for other_agent_info in observation[1:]:
+          _other_agent = other_agent_info
+          _other_agent_speed = SPEED # _other_agent['current_speed']
+          _other_agent_heading = _other_agent['current_heading']
+          _other_agent_velocity = [_other_agent_speed*np.cos(_other_agent_heading)
+                                  ,_other_agent_speed*np.sin(_other_agent_heading)]
+          ref_final_point = _other_agent['current_position'] - _other_agent['end'] #Point obj
+          x = np.array([ref_final_point.x, ref_final_point.y])
+          pref_vel = ORCA.normalized(-x) * _other_agent_speed
+
+          other_agent = ORCA.Agent(position =_other_agent['current_position'], 
+                          velocity = _other_agent_velocity,
+                          radius = _other_agent['radius'],
+                          max_speed = _other_agent['max_speed'],
+                          pref_velocity = pref_vel)
+          
+          candidates.append(other_agent)
         
-
-    def get_avoidance_velocity(self, agent, collider, t, dt): # -> change_of_velocity, vector
-        """Get the smallest relative change in velocity between agent and collider
-        that will get them onto the boundary of each other's velocity obstacle
-        (VO), and thus avert collision."""
-
-        # This is a summary of the explanation from the AVO paper.
-        #
-        # The set of all relative velocities that will cause a collision within
-        # time tau is called the velocity obstacle (VO). If the relative velocity
-        # is outside of the VO, no collision will happen for at least tau time.
-        #
-        # The VO for two moving disks is a circularly truncated triangle
-        # (spherically truncated cone in 3D), with an imaginary apex at the
-        # origin. It can be described by a union of disks:
-        #
-        # Define an open disk centered at p with radius r:
-        # D(p, r) := {q | ||q - p|| < r}        (1)
-        #
-        # Two disks will collide at time t iff ||x + vt|| < r, where x is the
-        # displacement, v is the relative velocity, and r is the sum of their
-        # radii.
-        #
-        # Divide by t:  ||x/t + v|| < r/t,
-        # Rearrange: ||v - (-x/t)|| < r/t.
-        #
-        # By (1), this is a disk D(-x/t, r/t), and it is the set of all velocities
-        # that will cause a collision at time t.
-        #
-        # We can now define the VO for time tau as the union of all such disks
-        # D(-x/t, r/t) for 0 < t <= tau.
-        #
-        # Note that the displacement and radius scale _inversely_ proportionally
-        # to t, generating a line of disks of increasing radius starting at -x/t.
-        # This is what gives the VO its cone shape. The _closest_ velocity disk is
-        # at D(-x/tau, r/tau), and this truncates the VO.
-
-        x = -(agent.position - collider.position)
-        v = agent.velocity - collider.velocity
-        # r -> agent size 
-        r = agent.radius + collider.radius
-
-        x_len_sq = self.norm_sq(x) # magnitude of x 
-
-        if x_len_sq >= r * r:
-            # We need to decide whether to project onto the disk truncating the VO
-            # or onto the sides.
-            #
-            # The center of the truncating disk doesn't mark the line between
-            # projecting onto the sides or the disk, since the sides are not
-            # parallel to the displacement. We need to bring it a bit closer. How
-            # much closer can be worked out by similar triangles. It works out
-            # that the new point is at x/t cos(theta)^2, where theta is the angle
-            # of the aperture (so sin^2(theta) = (r/||x||)^2).
-            adjusted_center = x/t * (1 - (r*r)/x_len_sq)
-
-            if dot(v - adjusted_center, adjusted_center) < 0:
-                # v lies in the front part of the cone
-                # print("front")
-                # print("front", adjusted_center, x_len_sq, r, x, t)
-                w = v - x/t
-                u = self.normalized(w) * r/t - w
-                n = self.normalized(w)
-            else: # v lies in the rest of the cone
-                # print("sides")
-                # Rotate x in the direction of v, to make it a side of the cone.
-                # Then project v onto that, and calculate the difference.
-                leg_len = sqrt(x_len_sq - r*r)
-                # The sign of the sine determines which side to project on.
-                sine = copysign(r, det((v, x)))
-                rot = array(
-                    ((leg_len, sine),
-                    (-sine, leg_len)))
-                rotated_x = rot.dot(x) / x_len_sq
-                n = perp(rotated_x)
-                if sine < 0:
-                    # Need to flip the direction of the line to make the
-                    # half-plane point out of the cone.
-                    n = -n
-                # print("rotated_x=%s" % rotated_x)
-                u = rotated_x * dot(v, rotated_x) - v
-                # print("u=%s" % u)
-        else:
-            # We're already intersecting. Pick the closest velocity to our
-            # velocity that will get us out of the collision within the next
-            # timestep.
-            # print("intersecting")
-            w = v - x/dt
-            u = self.normalized(w) * r/dt - w
-            n = self.normalized(w)
-        return u, n
-
+       new_vel, line = ORCA.orca(agent, candidates, self.tau, self.dt)
+       print(f'new_vel: {new_vel}') 
+       return new_vel
+       
         
-    def norm_sq(self, x):
-        return dot(x, x)
+       pass 
 
-    def normalized(self, x):
-        l = self.norm_sq(x)
-        assert l > 0, (x, l)
-        return x / sqrt(l)
 
-    def dist_sq(self, a, b):
-        return self.norm_sq(b - a)
 
--- gym-examples/gym_examples/envs/dynamics_orca.py
+++ gym-examples/gym_examples/envs/dynamics_orca.py
@@ -10,20 +10,28 @@ class ORCA_Dynamics(DynamicsTemplate):
         super().__init__(dt, is_learning)
 
 
-    def update(self, uav:UAV_v2_template, action):
+    def update(self, uav:UAV_v2_template, action): #new_vel
         # action coming from orca
         # is a vector delta_vx, delta_vy
         
         new_v_x = action[0]
+        print(f'new_vx:{new_v_x}')
         new_v_y = action[1]
+        print(f'new_vy: {new_v_y}')
         new_speed = math.sqrt(new_v_x**2 + new_v_y**2)
+        print(f'new speed: {new_speed}')
         new_heading = math.atan2(new_v_y, new_v_x)
 
+        print(f'current speed: {uav.current_speed}')
+        print(f'current position: {uav.current_position.x, uav.current_position.y}')
         _x = uav.current_position.x + (new_v_x * self.dt) #+ (0.5 * a_x * self.dt**2)
         _y = uav.current_position.y + (new_v_y * self.dt) #+ (0.5 * a_y * self.dt**2)
-
+        
+        
         uav.current_position = Point(_x, _y)
+        print(f'updated current position: {uav.current_position.x, uav.current_position.y}')
         uav.current_speed = new_speed
+        print(f'updated current speed: {uav.current_speed}')
         uav.current_heading = new_heading
 
         

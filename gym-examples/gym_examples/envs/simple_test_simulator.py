ba5bed9d99c79107b0b75016fa23ac11ae872d8a
chief-khanman <farhanaadit@gmail.com>
2025-03-28 03:13:46 -0500

    test script and testing changes

--- gym-examples/gym_examples/envs/simple_test_simulator.py
+++ gym-examples/gym_examples/envs/simple_test_simulator.py
@@ -1,36 +1,36 @@
-import torch
 from simple_env import SimpleEnv
-from models.LSTM_A2C_core import LSTM_A2C
+from utils_data_transform import process_obs
 
 
 
 
-env = SimpleEnv(obs_space_str='seq', sorting_criteria='closest first')
-obs, info= env.reset()
-other_agent_state_size = len(env._get_obs()['other_agents_states'])
-learning_agent_state_size = len(env._get_obs()) - 1
-lstm_hidden_size = 64
-action_size = env.action_space.shape[0]
+env = SimpleEnv(non_learning_uav_count=6,
+                obs_space_str='seq', 
+                sorting_criteria='closest first', 
+                max_number_other_agents_observed=2) #this defines how many other_agents the learning agent is looking at 
 
-# use a simple model from stable_baselines 
-# use learning_agent state only with model from stable_baselines 
-model = None #insert model from baselines  
+(obs, info), ep_ret, ep_len = env.reset(), 0, 0
 
+# print(f'obs: {obs}, \ninfo: {info}\nep_ret: {ep_ret}\nep_len: {ep_len}')
+# print(obs)
 
-#TODO: create method for agent state tensor -> use agent.get_state() and convert dict to tensor
-learning_agent_states = torch.randn((9))
-#TODO: 
-other_agents_states = torch.from_numpy(env._get_obs()['other_agents_states']).unsqueeze(0).to(torch.float32)
-
-# print(type(other_agents_states))
-
-action, value, logp = model(learning_agent_states, other_agents_states)
-
-print(action, value, logp)
+# process_obs splits obs into two lists, learning_agent, other_agents_states
+# learning agent holds mask data
+print(process_obs(obs)[0]) 
+print()
+print(process_obs(obs)[1])
 
 
+# print(env.observation_space['other_agent_state'].shape)
+# print(env.action_space.shape)
+print(env.max_number_other_agents_observed)
+print(env.get_obs_shape()['learning_agent_state_shape'], env.get_obs_shape()['other_agents_states_shape'])
 
+# for i in range(10):
+#     sample_action = 100 * env.action_space.sample()
+#     print(f'sample action: {sample_action}')
+    
+#     env.agent.dynamics.update(env.agent, sample_action)
+#     current_agent_state = env.agent.get_state()
 
-
-
-
+#     print(f'Agent state: {current_agent_state}')

12dba399fc90c00a38c100f0c66d58ea0bece0b4
chief-khanman <farhanaadit@gmail.com>
2025-04-08 20:27:11 -0500

    added methods for determining sound around UAV in MappedEnv, last two methods are incomplete

---
+++ gym-examples/gym_examples/envs/uam_sound.py
@@ -0,0 +1,177 @@
+import math
+import numpy as np
+import xarray as xr
+from geopandas import GeoDataFrame as GDF
+from shapely import Point
+
+
+
+
+def get_map_bounds(location_gdf:GDF):
+    '''For a given map GDF(GeoDataFrame) return the bounds
+    min_x, min_y, max_x, max_y.
+    
+    Args:
+        location GDF
+    
+    Returns:
+        (float, float, float, float)'''
+
+    # units returned from location_gdf are in meters
+    
+    # get bounds from airspace 
+    min_x, min_y, max_x, max_y = location_gdf.total_bounds
+    # floor min_x and min_y 
+    min_x , min_y = float(math.floor(min_x)), float(math.floor(min_y))
+    # ceil max_x and max_y
+    max_x, max_y = float(math.ceil(max_x)), float(math.ceil(max_y))
+
+    return min_x, min_y, max_x, max_y
+
+
+def create_noise_matrix(min_x, min_y, max_x, max_y, cell_dim = 100):
+    ''' Create noise matrix for each time step.
+        The rows and columns of this matrix depends on
+        map dimensions. Given map bounds return noise matrix.
+        
+        Args: 
+             minx, miny, maxx, maxy
+        
+        Returns:
+            ndarray
+        '''
+    
+    
+    rows, cols = (max_y - min_y)/cell_dim, (max_x - min_x)/cell_dim
+    noise_matrix = np.zeros((rows, cols))
+    
+    return noise_matrix
+
+
+
+def uav_array_window(window_ln, center_idx):
+    ''' Given window length, and window center index,
+        return indices of surrounding UAV. 
+        
+        Args: 
+            int, (int, int)
+        
+        Returns:
+        A list of array indices to store the noise intensity.
+            List[Tuple[int, int]]'''
+    
+    window = []
+    row_range = column_range = window_ln//2
+    for i in range(center_idx[0]-row_range, center_idx[0]+row_range+1):
+        temp_list = []
+        for j in range(center_idx[1]-column_range, center_idx[1]+column_range+1):
+            temp_list.append((i,j))
+        window.append(temp_list)
+
+    return window
+
+
+
+# position to array index
+def pos2idx(position:Point, minx, miny):
+    ''' Using position within map, 
+        return corresponding array index'''
+    # if a UAV's x position is 3.9999
+    # and its    y position is 6.7777
+    # using floor x is 3 and 
+    #             y is 6
+    # to get COLUMN index in array (x - minx) -> 3-3 = 0
+    # to get ROW    index in array (y - miny) -> 6-6 = 0
+    # so, UAV current_pos belongs to array_pos (row, col) -> (0,0)
+    #  
+    
+    x, y  = float(math.floor(position.x)), float(math.floor(position.y))
+    col = x - minx
+    row = y - miny
+    index = (row, col)
+    
+    return index
+
+
+
+def idx2pos(array_loc, minx, miny, cell_dim=100):
+    '''Provided an array index(location), 
+    this method will return centroid positions
+    of the corresponding array index(location)'''
+    # add half of cell dimension to find centroid location
+
+    row, col = array_loc[0], array_loc[1]
+
+    x = minx + col + cell_dim//2
+    y = miny + row + cell_dim//2
+    position = Point((x,y))
+    
+    return position
+
+
+# This method needs to be called inside another method,
+# where there is a mapping between noise_array and obs_location
+# because, we will only determine the sound 
+# for a given window around a UAVs current location  
+def get_noise_intensity(uav_current_pos, uav_current_speed, uav_rotor_speed, obs_location, altitude, noise_model=None):
+    '''
+    Given the (altitude) current position, speed and rotor speed, 
+    for a given observer location return the noise intensity. 
+    The arguments will be passed to a NN to provide the noise intensity
+    at observer location.
+
+    Args: 
+        uav_current_pos: Point,
+        uav_current_speed: float,
+        uav_rotor_speed: float,
+        obs_location: Point,
+        altitude: float
+    Returns:
+        float  
+    '''
+    if noise_model == None:
+        raise RuntimeError('noise model not passed to kwarg')
+    
+    # noise_model has to be a pytorch model
+    # add a check to ensure noise_model is a pytorch model
+
+    noise_intensity = noise_model(uav_current_pos, uav_current_speed, uav_rotor_speed, altitude, obs_location)
+
+    return noise_intensity
+    
+
+    
+
+
+def calculate_noise_window(noise_window, minx, miny, noise_intensity = get_noise_intensity):
+    # for a noise matrix and a defined noise window associated with a UAV
+    # calculate noise level for the UAV's noise window
+    #  
+    idx_list = []
+    for _list in noise_window:
+        for item in _list:
+            idx_list.append(item)
+
+    location_list = []
+    for item in idx_list:
+        position = idx2pos(item, minx, miny)
+        location_list.append(position)
+    
+    for location in location_list:
+        pass
+    
+    
+
+    
+    pass
+
+def set_noise_matrix():
+    # update the noise matrix given noise window from a UAV
+    pass
+
+
+
+
+if __name__ == '__main__':
+    some_window = uav_array_window(5,(1,1))
+    print(some_window)

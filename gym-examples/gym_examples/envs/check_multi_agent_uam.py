8c30ca229b27ee3fb1dbb507ec7035889e0f8cb3
chief-khanman <farhanaadit@gmail.com>
2025-04-10 15:58:00 -0500

    deleted files that are not related to Simple_Env

--- gym-examples/gym_examples/envs/check_multi_agent_uam.py
+++
@@ -1,7 +0,0 @@
-from multi_uam_uav import UamUavEnvPZ
-
-from pettingzoo.test import parallel_api_test
-
-
-env = UamUavEnvPZ("Austin, Texas, USA", 8, 5, sleep_time=0.01)
-parallel_api_test(env, num_cycles=100)

31d77a96fdb74ea37f9d151113b946f59bbe6e1d
chief-khanman <farhanaadit@gmail.com>
2025-04-10 16:25:27 -0500

    deleted unused imports

--- gym-examples/gym_examples/envs/utils.py
+++ gym-examples/gym_examples/envs/utils.py
@@ -1,17 +1,14 @@
-import geopandas as gpd
-from matplotlib.axes._axes import Axes
-from simulator import Simulator
 from uav_v2 import UAV_v2
 import numpy as np
 
 
 
-def static_plot(sim_obj: Simulator, ax: Axes, gpd: gpd) -> None:
-    sim_obj.airspace.location_utm_gdf.plot(ax=ax, color="gray", linewidth=0.6)
-    sim_obj.airspace.location_utm_hospital_buffer.plot(ax=ax, color="green", alpha=0.3)
-    sim_obj.airspace.location_utm_hospital.plot(ax=ax, color="black")
-    # adding vertiports to static plot
-    gpd.GeoSeries(sim_obj.sim_vertiports_point_array).plot(ax=ax, color="black")
+# def static_plot(sim_obj: Simulator, ax: Axes, gpd: gpd) -> None:
+#     sim_obj.airspace.location_utm_gdf.plot(ax=ax, color="gray", linewidth=0.6)
+#     sim_obj.airspace.location_utm_hospital_buffer.plot(ax=ax, color="green", alpha=0.3)
+#     sim_obj.airspace.location_utm_hospital.plot(ax=ax, color="black")
+#     # adding vertiports to static plot
+#     gpd.GeoSeries(sim_obj.sim_vertiports_point_array).plot(ax=ax, color="black")
 
 
 

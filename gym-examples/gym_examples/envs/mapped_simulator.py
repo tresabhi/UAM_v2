9fcd0bff214d4a65d992ddcf764c5c8a4dfac815
chief-khanman <farhanaadit@gmail.com>
2025-05-23 17:16:43 -0500

    new script for running stand-alone simulator without training agents(agent being trained require the gym env)

---
+++ gym-examples/gym_examples/envs/mapped_simulator.py
@@ -0,0 +1,31 @@
+'''
+This script is the simulator script. 
+Basis for simulation.
+
+Instance of this will be used for simulation(without learned agents)
+'''
+
+
+
+class MappedSimulator:
+    def __init__(self, ):
+        pass 
+
+
+    def reset(self):
+        pass
+
+
+    def step(self):
+        pass
+
+
+
+    def render(self):
+        pass
+
+
+    def close(self):
+        pass
+
+

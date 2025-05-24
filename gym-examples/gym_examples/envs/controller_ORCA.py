b93c024fdeb29d963d5e67440e3f300fa5a8ce50
chief-khanman <farhanaadit@gmail.com>
2025-05-23 23:19:00 -0500

    fixed definitions and returns for ORCA agent

--- gym-examples/gym_examples/envs/controller_ORCA.py
+++ gym-examples/gym_examples/envs/controller_ORCA.py
@@ -12,19 +12,11 @@ class ORCA_controller(ControllerTemplate):
     which will calculate necessary attribute - new velocity.'''
 
     def __init__(self, max_acceleration, max_heading_change):
-        
-
-
-
-        pass 
+        self.max_acceleration = max_acceleration
+        self.max_heading_change = max_heading_change
 
 
 
 
     def __call__(self, observation):
-        # take a step in RVO2
-        # collect and send new position to dynamics 
-        # uav.dynamics will not perform any calculations related to accleration and instantaneous heading change using these values 
-        # uav.dynamics will perform some calculation to convert velocity to speed, direction, new position and new current heading
-        # uav.dynamics will send values directly to UAV to update position and heading
-        pass 
+        return (0,0) 

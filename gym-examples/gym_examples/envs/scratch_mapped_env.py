2f399f3fc94041b1c91e4abb6c5f6c2fba97d710
chiefkhanman <farhanaadit@gmail.com>
2025-07-03 14:55:22 -0500

    added simple_controller to test AutoUAV dynamics

--- gym-examples/gym_examples/envs/scratch_mapped_env.py
+++ gym-examples/gym_examples/envs/scratch_mapped_env.py
@@ -1,11 +1,39 @@
 import numpy as np 
+import math
 import matplotlib.pyplot as plt
 
 import random
+import shapely
 from map_renderer import MapRenderer
 from map_logging_loader import MapLoader
 from map_env_revised import MapEnv
 from rewards_utils import _get_reward_only_agent
+from utils_data_transform import normalize_minus_one_one, normalize_zero_one
+
+
+
+
+
+def simple_controller(current_position:shapely.Point, final_position:shapely.Point, current_heading:float):
+    
+    acceleration = np.random.uniform(0,1)
+    final_heading = math.atan2(final_position.y - current_position.y, 
+                                final_position.x - current_position.x)
+    
+    heading_change_rad = final_heading - current_heading
+    
+    heading_change = normalize_minus_one_one(heading_change_rad, -math.pi, math.pi)
+
+    action = np.array([acceleration, heading_change])
+    
+    return action
+
+
+
+
+
+
+
 
 
 def simple_reward(self):
@@ -15,7 +43,7 @@ number_orca_agents = 0
 number_uav = 0
 number_of_vp = 8
 episodes = 1
-max_steps_per_episode = 4
+max_steps_per_episode = 30
 render = False
 save_animation = False
 env_seed = 42
@@ -23,7 +51,7 @@ episode_seeds = 60
 mp4_only = False
 
 
-MapEnv._get_reward = simple_reward
+MapEnv._get_reward = _get_reward_only_agent
 
 env = MapEnv(
             number_of_uav= number_uav,
@@ -40,17 +68,55 @@ env = MapEnv(
             max_vertiports=150, #set these as some hyperparameters 
         )
 
-print('The following is a sample of observation space:')
-print(env.observation_space.sample())
 
-print()
+
+
+# print('The following is a sample of observation space:')
+# print(env.observation_space.sample())
+
+# print('sample action:')
+# sample_action = env.action_space.sample()
+# print(sample_action)
+# sa_type = type(sample_action)
+# print('Type:', sa_type)
+
+# print(sample_action.shape)
+
+# env.reset()
+
+# print('Raw obs: ')
+# print(env.agent.get_obs()) # <- this is raw obs 
+
+
+
+
+
+
 for episode in range(episodes):
     obs, info = env.reset(seed=episode_seeds)
 
-    for step in range(max_steps_per_episode):
-        action = env.action_space.sample()
+    print('Observation: ')
+    print(obs)
 
-        next_obs, reward, terminated, truncated, info = env.step(action)
+    print('Info:')
+    print(info)
+
+    for step in range(max_steps_per_episode):
+        # action = env.action_space.sample()
+        action = simple_controller(env.agent.current_position, env.agent.end, env.agent.current_heading)
+
+        obs, reward, terminated, truncated, info = env.step(action)
+        
+
+        print('Observation: ')
+        print(obs)
+
+        print('Reward:')
+        print(reward)
+
+        print('Info:')
+        print(info)
+
  
         if terminated:
             print('Terminated')
@@ -63,3 +129,20 @@ for episode in range(episodes):
                 print(f"Episode timeout after {step} steps!")
             break
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

5adb136ed80d6558eccab5809936e364442a20c4
chiefkhanman <farhanaadit@gmail.com>
2025-07-07 01:02:50 -0500

    simulator script for future improvement

---
+++ gym-examples/gym_examples/envs/simulator.py
@@ -0,0 +1,26 @@
+class UAM_Simulator():
+    def __init__(self,
+                 num_UAV,
+                 num_ORCA,
+                 num_vertiport,):
+        pass
+
+
+    def step(self,):
+        pass
+
+
+    def reset(self):
+        pass
+
+
+    def render(self):
+        pass
+
+
+    def close(self):
+        pass
+
+
+    def log(self):
+        pass
